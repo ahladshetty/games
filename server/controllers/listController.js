7418fa75415eb4ba5e651090a89b8592192a37cb
ahladshetty <ahladshetty03@gmail.com>
2024-03-31 09:24:57 +0530

    all lists, list info, update, delete

--- server/controllers/listController.js
+++ server/controllers/listController.js
@@ -1,7 +1,6 @@
 import List from "../models/List.js";
 
 // ROUTE 1: list games using GET '/games'
-
 export const createList = async (req, res) => {
   try {
     const { title, description, gameIds } = req.body;
@@ -15,5 +14,65 @@ export const createList = async (req, res) => {
     res.status(201).json(newGameList);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// ROUTE 2: get all lists using GET '/lists'
+export const getLists = async (req, res) => {
+  try {
+      const lists = await List.find();
+      res.status(200).json(lists);
+  } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// ROUTE 3: get list by id using GET '/listinfo/:id'
+export const getListById = async (req, res) => {
+  try {
+      const list = await List.findById(req.params.id);
+      if (!list) {
+          return res.status(404).json({ error: "List not found" });
+      }
+      res.status(200).json(list);
+  } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// ROUTE 4: update list using PATCH '/updatelist/:id'
+export const updateList = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const { title, description, gameIds } = req.body;
+
+      const updatedList = await List.findByIdAndUpdate(id, { title, description, games: gameIds }, { new: true });
+      
+      // { new: true } returns updated list
+
+      if (!updatedList) {
+          return res.status(404).json({ error: "List not found" });
+      }
+
+      res.status(200).json(updatedList);
+  } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// ROUTE 5: delete list using DELETE '/deletelist/:id'
+export const deleteList = async (req, res) => {
+  try {
+      const { id } = req.params;
+
+      const deletedList = await List.findByIdAndDelete(id);
+
+      if (!deletedList) {
+          return res.status(404).json({ error: "List not found" });
+      }
+
+      res.status(200).json({ message: "List deleted successfully" });
+  } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- server/routes/listRoutes.js
+++ server/routes/listRoutes.js
@@ -1,12 +1,17 @@
 import express from 'express';
 
-import { createList } from '../controllers/listController.js'
+import { createList, getLists, getListById, updateList, deleteList } from '../controllers/listController.js';
 import userAuth from '../middleware/auth.js'
 
 const router = express.Router();
 
 router.post('/createlist', createList);
 
+router.get('/lists', getLists);
+router.get('/listinfo/:id', getListById);
+
+router.patch('/updatelist/:id', updateList); // for partial update
+
+router.delete('/deletelist/:id', deleteList);
+
 export default router;
-
-
